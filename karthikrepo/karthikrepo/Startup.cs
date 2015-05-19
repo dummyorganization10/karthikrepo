81bc92b899c82a1620b6cce414b3a2207063220f
karthik venna <sailfishvideos@gmail.com>
2015-05-19 11:18:26 +0530

    123456

---
+++ karthikrepo/karthikrepo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(karthikrepo.Startup))]
+namespace karthikrepo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
